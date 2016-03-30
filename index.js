5268149c2d04903661e289e4e1479669608af193
alexdecapri <alexdecapri@gmail.com>
2016-03-29 20:50:01 -0600

    finishing my day 1

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var cors = require("cors");
-var mongoJS = require("mongojs");
+var mongojs = require("mongojs");
 // var mongoose = require("mongoose");
 
 // Express
@@ -18,7 +18,11 @@ app.listen(port, function() {
   console.log("Listening on port: " + port);
 });
 
-// Mongo
+// Mongo (using mongoJS)
+var db = mongojs("ecommerce", ["products"]); //products is a collection of the ecommerce database
+// var Product = db.products; is this right?
+
+// Mongo (using mongoose)
 // var Schema = mongoose.Schema;
 // var mongoUri = "mongodb://localhost:27017/ecommerce";
 // mongoose.set("debug", true);
@@ -41,7 +45,10 @@ app.get("/api/products/:id", function(req, res) {
   res.send("Test GET for /api/products using req.params.id!");
 });
 app.post("/api/products", function(req, res) {
-  res.send(req.body);
+  db.products.insert(req.body, function(err, result) {
+    if (err) res.status(500).send(err);
+    else res.json(result);
+  });
 });
 app.put("/api/products/:id", function(req, res) {
   res.send("Test PUT for /api/products using req.params.id!");
